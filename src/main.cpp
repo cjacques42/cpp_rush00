280d486a4a6158a920b0776ce830b436b548c393
Eliott GABOREAU <egaborea@e2r4p7.42.fr>
2017-04-08 13:05:19 +0200

    J'ai essaye des trucs mais ca ne mache pas je vais manger deso

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <curses.h>
 
+
+
+WINDOW *actualize_window(WINDOW *old_win) {
+	WINDOW	*win = newwin(50, COLS - 2 , (LINES - 50) / 2, 1);
+
+	delwin(old_win);
+
+	return win;
+}
+
+
 int		main(){
 	bool	exit = false;
 
@@ -7,18 +18,27 @@ int		main(){
 	raw();				/* Line buffering disabled	*/
 	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
 	noecho();			/* Don't echo() while we do getch */
-	nodelay(stdscr, TRUE);
+	nodelay(stdscr, TRUE); /* Doesn't wait for user to press a key*/
 	int ch;
 
+	WINDOW	*win = newwin(50, COLS - 2 , (LINES - 50) / 2, 1);
+
+	wborder(win, 0, 0, 0, 0, 0, 0, 0, 0);
 	while(!exit) {
 		ch = getch();
 
-		if (ch == 4 || ch == 3){
+		if (ch == 4 || ch == 3 || ch == 27){
 			exit = true;
+		} else if (ch == 410) {// corresponds to the user changing the size of the screen
+			win = actualize_window(win);
+			wborder(win, 0, 0, 0, 0, 0, 0, 0, 0);
+			wrefresh(win);
 		} else if (ch != ERR){
-			printw("%d", ch);
-		} 
+			printw("%d\n", ch);
 
+			box(win, 0 , 0);
+		}
+		wrefresh(win);
 	}
 
 
